2024c3d6974b89fb64279b736e9f72fc9f125eac
Son Nguyen <ntson.sh@gmail.com>
2017-03-08 08:20:46 +0700

    'Service'

---
+++ angular-tour-of-heroes/src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. A' },
+    { id: 12, name: 'Mr. B' },
+    { id: 13, name: 'Mr. C' },
+    { id: 14, name: 'Mr. D' },
+    { id: 15, name: 'Mr. E' },
+    { id: 16, name: 'Mr. F' },
+    { id: 17, name: 'Mr. G' },
+    { id: 18, name: 'Mr. H' },
+    { id: 19, name: 'Mr. I' },
+    { id: 20, name: 'Mr. J' },
+];
+//# sourceMappingURL=mock-heroes.js.map
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. A'},
+    { id: 12, name: 'Mr. B'},
+    { id: 13, name: 'Mr. C'},
+    { id: 14, name: 'Mr. D'},
+    { id: 15, name: 'Mr. E'},
+    { id: 16, name: 'Mr. F'},
+    { id: 17, name: 'Mr. G'},
+    { id: 18, name: 'Mr. H'},
+    { id: 19, name: 'Mr. I'},
+    { id: 20, name: 'Mr. J'},
+  ];
