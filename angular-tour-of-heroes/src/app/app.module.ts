926afba7ecc307acb471496197d088ad99a6a271
sonnguyen <sonnguyen>
2017-03-08 17:40:26 +0700

    Routing - redirect the main page to dashboard page

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,13 +1,42 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroService } from './hero.service';
+
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        // Redirect when access to to root page.
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+
+      },
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent
+    }
+  ])
+   ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers: [ HeroService ],
+  bootstrap:    [ AppComponent ],
+  
 })
 export class AppModule { }
--- angular-tour-of-heroes/src/app/dashboard.component.js
+++ angular-tour-of-heroes/src/app/dashboard.component.js
@@ -9,26 +9,17 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var mock_heroes_1 = require('./mock-heroes');
-var HeroService = (function () {
-    function HeroService() {
+var DashboardComponent = (function () {
+    function DashboardComponent() {
     }
-    HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
-    };
-    // For testing slow connection
-    HeroService.prototype.getHeroesSlowly = function () {
-        var _this = this;
-        return new Promise(function (resolve) {
-            // Simulate server latency with 2 second delay
-            setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
-        });
-    };
-    HeroService = __decorate([
-        core_1.Injectable(), 
+    DashboardComponent = __decorate([
+        core_1.Component({
+            selector: 'my-dashboard',
+            template: "<h3>My Dashboard</h3>"
+        }), 
         __metadata('design:paramtypes', [])
-    ], HeroService);
-    return HeroService;
+    ], DashboardComponent);
+    return DashboardComponent;
 }());
-exports.HeroService = HeroService;
-//# sourceMappingURL=hero.service.js.map
+exports.DashboardComponent = DashboardComponent;
+//# sourceMappingURL=dashboard.component.js.map
